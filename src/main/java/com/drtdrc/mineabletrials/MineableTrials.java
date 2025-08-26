3aced91cbbe5920e8f2851604ab654dc100c5506
crdtrd <crdtrd@gmail.com>
2025-08-25 21:51:42 -0700

    Initial commit

---
+++ src/main/java/com/drtdrc/mineabletrials/MineableTrials.java
@@ -0,0 +1,10 @@
+package com.drtdrc.mineabletrials;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MineableTrials implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
