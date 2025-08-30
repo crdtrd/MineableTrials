88d14ff695df32c02013dcb21dcc906c6f50f7de
crdtrd <crdtrd@gmail.com>
2025-08-30 15:34:50 -0700

    Added TrialSpawnerStateMixin

---
+++ src/main/java/com/drtdrc/mineabletrials/mixin/TrialSpawnerStateMixin.java
@@ -0,0 +1,9 @@
+package com.drtdrc.mineabletrials.mixin;
+
+import net.minecraft.block.enums.TrialSpawnerState;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(TrialSpawnerState.class)
+public class TrialSpawnerStateMixin {
+
+}
